e6e917863c2043aca0c7f4a285ff638af8c7f5c7
Chris <cris_cgc_22@hotmail.com>
2021-10-19 20:25:11 +0200

    Cambios configuracion
    cambios

---
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/Configuration.kt
@@ -0,0 +1,11 @@
+package com.appverse.hitthecooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Configuration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuration)
+    }
+}
