7797772a44b6f40826c1088f5d70cd3ec48cad0a
Sergio <sergiolopeztorres97@gmail.com>
2021-10-21 20:27:48 +0200

    Pantalla de listas de la compra
    Pendiente de añadir varias cosas, además de añadir efecto al pulsar cardview y layout

--- HitTheCooks/app/src/main/java/com/appverse/hitthecooks/ShoppingListActivity.kt
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/ShoppingListActivity.kt
@@ -2,10 +2,19 @@ package com.appverse.hitthecooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.appverse.hitthecooks.databinding.ActivityShoppingListBinding
 
 class ShoppingListActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityShoppingListBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping_list)
+        setContentView(binding.root)
+
+
+
+        binding.shoppingList.setOnClickListener {
+            Toast.makeText(this,"prueba",Toast.LENGTH_SHORT).show()
+        }
     }
 }
