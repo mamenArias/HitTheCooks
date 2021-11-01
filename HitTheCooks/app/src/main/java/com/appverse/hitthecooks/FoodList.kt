89ebcc2286b95b1dd8090db1f8b1850e4c9cce97
Mamen <mcariasdeharo@gmail.com>
2021-11-01 18:29:55 +0100

    Cambio de Pantalla
    Inserción de los diferentes cambios de pantalla desde la de Food List

--- HitTheCooks/app/src/main/java/com/appverse/hitthecooks/FoodList.kt
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/FoodList.kt
@@ -1,11 +1,37 @@
 package com.appverse.hitthecooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.appverse.hitthecooks.databinding.ActivityFoodListBinding
 
 class FoodList : AppCompatActivity() {
+
+    private val binding by lazy { ActivityFoodListBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_list)
+        setContentView(binding.root)
+
+        binding.buttonBack.setOnClickListener {
+            val intent = Intent(this, ShoppingListActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonUser.setOnClickListener {
+            val intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonConfiguration.setOnClickListener {
+            val intent = Intent(this, ConfigurationActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
+
 }
