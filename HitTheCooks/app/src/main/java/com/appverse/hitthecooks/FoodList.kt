87ab6e102e38882658fe6349e256e3865d938c40
Mamen <mcariasdeharo@gmail.com>
2021-12-09 20:43:38 +0100

    Cambios Firebase

--- HitTheCooks/app/src/main/java/com/appverse/hitthecooks/FoodList.kt
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/FoodList.kt
@@ -2,15 +2,23 @@ package com.appverse.hitthecooks
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.appverse.hitthecooks.databinding.ActivityFoodListBinding
 import com.appverse.hitthecooks.model.Item
 import com.appverse.hitthecooks.recyclers.FoodListAdapter
+import com.appverse.hitthecooks.utils.FirestoreCollections
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class FoodList : SuperActivity() {
 
     private val binding by lazy { ActivityFoodListBinding.inflate(layoutInflater) }
+    private val db= FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +41,24 @@ class FoodList : SuperActivity() {
             finish()
         }
 
-        binding.buttonConfiguration.setOnClickListener {
-            val intent = Intent(this, ConfigurationActivity::class.java)
+        binding.buttonAdd.setOnClickListener {
+            val intent = Intent(this, InvitationActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
             finish()
         }
 
+        db.collection(FirestoreCollections.USERS).document(Firebase.auth.currentUser!!.email.toString()).get().addOnSuccessListener {
+            Glide.with(this).load(it.get("profileImage")).circleCrop().into(binding.buttonUser as ImageView)
+        }
+
+        /*val storage:FirebaseStorage = FirebaseStorage.getInstance()
+        val gsReference = storage.getReferenceFromUrl("gs://hit-the-cooks.appspot.com/iconosLista/iconoA.png")
+        gsReference.child("iconosLista/iconoA.png").downloadUrl.addOnSuccessListener {
+
+        }.addOnFailureListener {
+
+        }*/
         val arrayItems:ArrayList<Item> = arrayListOf()
         arrayItems.add(Item("Patatas", "https://firebasestorage.googleapis.com/v0/b/hit-the-cooks.appspot.com/o/iconosLista%2Ficonocongelados.png?alt=media&token=125754f0-80e7-435f-a651-2cfab382d52a"))
         arrayItems.add(Item("Patatas", "https://firebasestorage.googleapis.com/v0/b/hit-the-cooks.appspot.com/o/iconosLista%2Ficonocongelados.png?alt=media&token=125754f0-80e7-435f-a651-2cfab382d52a"))
