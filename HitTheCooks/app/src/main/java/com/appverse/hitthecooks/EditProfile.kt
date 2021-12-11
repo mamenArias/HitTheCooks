826dc7ad4d097375ad1b6665e63fc5918f8256a9
Sergio <sergiolopeztorres97@gmail.com>
2021-12-11 11:49:01 +0100

    cambio en un import

--- HitTheCooks/app/src/main/java/com/appverse/hitthecooks/EditProfile.kt
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/EditProfile.kt
@@ -20,6 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
