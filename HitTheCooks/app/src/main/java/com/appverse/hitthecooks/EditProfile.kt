4bd803fa4f99e81f6d36e8bb741de14c008c6719
Miguel Ángel <moneite@gmail.com>
2021-12-13 17:53:51 +0100

    Update EditProfile.kt

--- HitTheCooks/app/src/main/java/com/appverse/hitthecooks/EditProfile.kt
+++ HitTheCooks/app/src/main/java/com/appverse/hitthecooks/EditProfile.kt
@@ -90,37 +90,24 @@ class EditProfile : SuperActivity() {
         //Obtiene los datos del usuario actualmente logado
         db.collection(FirestoreCollections.USERS)
             .document(Firebase.auth.currentUser!!.email.toString()).get().addOnSuccessListener {
-            //Recupera el nombre y la imagen del usuario
-            binding.userName.text = it.get("email").toString()
-            Glide.with(this).load(it.get("profileImage")).circleCrop()
-                .into(binding.profileIcon as ImageView)
-            binding.profileIcon.visibility = View.VISIBLE
-            binding.userName.visibility = View.VISIBLE
-        }
-            Glide.with(this).load(it.get("profileImage")).circleCrop().into(binding.profileIcon as ImageView)
+                //Recupera el nombre y la imagen del usuario
+                binding.userName.text = it.get("email").toString()
+                Glide.with(this).load(it.get("profileImage")).circleCrop()
+                    .into(binding.profileIcon as ImageView)
+                binding.profileIcon.visibility = View.VISIBLE
+                binding.userName.visibility = View.VISIBLE
 
-            binding.profileIcon.visibility = View.VISIBLE
+                Glide.with(this).load(it.get("profileImage")).circleCrop()
+                    .into(binding.profileIcon as ImageView)
 
-        }
+                binding.profileIcon.visibility = View.VISIBLE
+            }
 
-        binding.newPhoto!!.setOnClickListener{
 
-                if (!binding.userName.text.toString().endsWith("gmail.com")) {
-                    //checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
-                } else {
-                    Toast.makeText(
-                        this,
-                        "No puedes cambiar la foto de un perfil de Gmail",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+        binding.newPhoto!!.setOnClickListener {
 
-        /**
-         * Al hacer click en la imagen, permite acceder a la galeria del movil y usar una imagen de la misma como perfil
-         */
-        binding.profileIcon.setOnClickListener {
             if (!binding.userName.text.toString().endsWith("gmail.com")) {
+                //checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -134,27 +121,47 @@ class EditProfile : SuperActivity() {
                 ).show()
             }
 
+            /**
+             * Al hacer click en la imagen, permite acceder a la galeria del movil y usar una imagen de la misma como perfil
+             */
+            binding.profileIcon.setOnClickListener {
+                if (!binding.userName.text.toString().endsWith("gmail.com")) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        STORAGE_PERMISSION_CODE
+                    )
+                } else {
+                    Toast.makeText(
+                        this,
+                        "No puedes cambiar la foto de un perfil de Gmail",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+
+            }
+
+            /**
+             * Función que permite navegar a la actividad principal
+             */
+            binding.goBackButton.setOnClickListener {
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                finish()
+            }
+
+            /**
+             * Despliega el menú de navegación lateral
+             */
+            binding.menuButton.setOnClickListener {
+                super.drawerLayout.openDrawer(GravityCompat.START)
+            }
 
         }
-
-        /**
-         * Función que permite navegar a la actividad principal
-         */
-        binding.goBackButton.setOnClickListener {
-            val intent: Intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(intent)
-            finish()
-        }
-
-        /**
-         * Despliega el menú de navegación lateral
-         */
-        binding.menuButton.setOnClickListener {
-            super.drawerLayout.openDrawer(GravityCompat.START)
-        }
-
     }
+
 
     /**
      * Función para coger una foto de la galería
@@ -179,10 +186,9 @@ class EditProfile : SuperActivity() {
 
                 binding.profileIcon.setImageURI(image)
 
+
                 val reference = FirebaseStorage.getInstance()
                     .getReference("imagenesPerfil/" + auth.currentUser!!.email.toString() + ".jpg")
-
-              val reference= FirebaseStorage.getInstance().getReference("imagenesPerfil/"+auth.currentUser!!.email.toString()+".jpg")
 
                 reference.putFile(image).addOnSuccessListener {
                     Toast.makeText(this, "Exito", Toast.LENGTH_SHORT).show()
@@ -195,58 +201,52 @@ class EditProfile : SuperActivity() {
                     var user = User(binding.userName.text.toString(), url.toString())
                     db.collection(FirestoreCollections.USERS)
                         .document(auth.currentUser!!.email.toString()).set(
-                        user
-                    )
+                            user
+                        )
                 }.addOnFailureListener { it ->
                     Toast.makeText(this, "Fallo bbdd", Toast.LENGTH_SHORT).show()
                     Log.d("Fallo BD", "" + it.stackTrace)
-                /*val referenceImages = FirebaseStorage.getInstance().getReference("imagenesPerfil").child(""+"".startsWith(auth.currentUser!!.email.toString()))
+                    reference.downloadUrl.addOnSuccessListener { url ->
+                        var user = User(binding.userName.text.toString(), url.toString())
+                        db.collection(FirestoreCollections.USERS)
+                            .document(auth.currentUser!!.email.toString()).set(
+                                user
+                            )
+                    }.addOnFailureListener { it ->
+                        Toast.makeText(this, "Fallo bbdd", Toast.LENGTH_SHORT).show()
 
-
-                referenceImages.downloadUrl.addOnSuccessListener {url->
-                    var userToInsert = User(auth.currentUser!!.email.toString(),url.toString())
-
-                    db.collection( FirestoreCollections.USERS).document(auth.currentUser!!.email.toString()).
-                }*/
-
-                //storageRef =
-                   // dbStorage.reference.child("imagenesPerfil").child(auth.currentUser!!.email.toString()+".jpg")
-
-               /* PARTE BUENA*/
-
-               reference.downloadUrl.addOnSuccessListener { url ->
-                 var user = User(binding.userName.text.toString(), url.toString())
-                    db.collection(FirestoreCollections.USERS).document(auth.currentUser!!.email.toString()).set(
-                       user
-                    )}.addOnFailureListener { it ->
-                    Toast.makeText(this, "Fallo bbdd", Toast.LENGTH_SHORT).show()
+                    }
 
                 }
-
             } else {
                 Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
             }
         }
     )
 
-    /**
-     * Sobreescribe la función de recibir permisos para, en caso de que se acepte, inicie la acción directamente
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //Realiza la acción
-                Toast.makeText(this, "Permisos garantizados", Toast.LENGTH_LONG).show()
-                selectImage()
-            } else {
-                //Informa sobre la denegación del permiso
-                Toast.makeText(this, "Permisos denegados", Toast.LENGTH_LONG).show()
-            }
-        }
-    }
+
+
+
+                /**
+                 * Sobreescribe la función de recibir permisos para, en caso de que se acepte, inicie la acción directamente
+                 */
+                override fun onRequestPermissionsResult(
+                    requestCode: Int,
+                    permissions: Array<out String>,
+                    grantResults: IntArray
+                ) {
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                    if (requestCode == STORAGE_PERMISSION_CODE) {
+                        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            //Realiza la acción
+                            Toast.makeText(this, "Permisos garantizados", Toast.LENGTH_LONG).show()
+                            selectImage()
+                        } else {
+                            //Informa sobre la denegación del permiso
+                            Toast.makeText(this, "Permisos denegados", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+
+
 }
